25938e3fde0a2ff55f0d2573b33977da7c29589d
Gustavo Crespo <Guticresp@hotmail.com>
2024-07-17 20:37:41 -0300

    docker funcionando

--- server.js
+++ server.js
@@ -22,10 +22,11 @@ db.connect((err) => {
 app.get('/', (req, res) => {
   db.query('SELECT NOW() as now', (err, result) => {
     if (err) {
-      res.status(500).send('Error querying database');
+      console.error('Erro ao executar a consulta:', err);
+      res.status(500).send('Erro ao tentar acessar a base. Verifique as informações dentro do seu banco de dados. Estamos utilizando o usupario ROOT, senha 1234 e database docker.');
       return;
     }
-    res.send(`Current time: ${result[0].now}`);
+    res.send(`Horário Atual: ${result[0].now}`);
   });
 });
 
